13ce40d73f0b28102e7b6e4d24eace2dcf122cf0
Xavi <xcatchot@gmail.com>
2022-11-23 19:15:19 +0200

    i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,29 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
   ssr: true,
   modules: [
     '@nuxt/content',
+    '@nuxtjs/i18n',
   ],
+  i18n: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    defaultLocale: 'en',
+    detectBrowserLanguage: false,
+    lazy: true,
+    langDir: 'locales',
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-GB',
+        name: 'English',
+        file: 'en-GB.json'
+      },
+      {
+        code: 'fi',
+        iso: 'fi-FI',
+        name: 'Suomi',
+        file: 'fi-FI.json'
+      }
+    ],
+    parsePages: false
+  },
 })
